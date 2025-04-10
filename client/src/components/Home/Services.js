78c606ffd1c4e35de675f795e6edf96978135269
Dawood Khan <110541711+Iyisku@users.noreply.github.com>
2025-04-10 12:42:41 +0500

    3rd and 4th page design done

--- client/src/components/Home/Services.js
+++ client/src/components/Home/Services.js
@@ -1,5 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react'
-import Bkg from '../../assets/Bck.png'
+import Heritagecottage from '../../assets/heritagecottage.png'
+import Jeepview from '../../assets/jeeptoview.png'
+import outdoorcamping from '../../assets/outdoorcamping.png'
 
 function Services() {
   const scrollContainer = useRef(null);
@@ -11,27 +13,27 @@ function Services() {
     {
       id: 1,
       name: 'Heritage Cottage Room',
-      image: Bkg, // Use the imported image directly
+      image: Heritagecottage, // Use the imported image directly
     },
     {
       id: 2,
       name: 'Outdoor Camping',
-      image: Bkg, // Applied to all images
+      image: outdoorcamping, // Applied to all images
     },
     {
       id: 3,
       name: 'Jeep 4D ViewPoint',
-      image: Bkg, // Applied to all images
+      image: Jeepview, // Applied to all images
     },
     {
       id: 4,
       name: 'Luxury Spa',
-      image: Bkg, // Applied to all images
+      image: "https://imgs.search.brave.com/dYtVWsiKHqdeK58mFQGfCgGHK2GpLkwtOssIultaqow/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9w/cm9kdWN0cy1yZWFk/eS1zcGEtc2Vzc2lv/bl8yMy0yMTUxOTE2/NTQyLmpwZz9zZW10/PWFpc19oeWJyaWQ", // Applied to all images
     },
     {
       id: 5,
       name: 'Fine Dining',
-      image: Bkg, // Applied to all images
+      image: "https://imgs.search.brave.com/Tfsn9Cl24qs7dpdQSadi7tDgKH1OLikWGefAJbaRgA4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA3LzEzLzA3LzMw/LzM2MF9GXzcxMzA3/MzA3OF9EZXNhT1Zj/NFR0b29Eb29UMDho/a2tHZE5BdUtjNXFr/Uy5qcGc", // Applied to all images
     }
   ];
 
@@ -93,12 +95,12 @@ function Services() {
     <div className="max-w-6xl mx-auto p-4">
       <div className="flex flex-col md:flex-row gap-6 mb-8">
         <div className="md:w-1/3">
-          <h2 className="text-3xl font-light text-gray-800 mb-1">Facilities and</h2>
-          <h2 className="text-3xl font-light text-gray-800">Services</h2>
+          <h2 className="text-3xl font-[400] font-jeju text-gray-800 mb-1">Facilities and</h2>
+          <h2 className="text-3xl font-[400] font-jeju text-gray-800">Services</h2>
         </div>
         
-        <div className="md:w-2/3">
-          <p className="text-gray-600">
+        <div className="md:w-2/3 pt-1">
+          <p className="text-gray-600 font-jeju text-lg text-left">
             Indulge in a world of luxury and comfort with our exclusive resort amenities,
             designed to create unforgettable experiences for you. Our Sanctuary of Luxury
             and comfort awaits your arrival
@@ -127,15 +129,15 @@ function Services() {
               key={facility.id} 
               className="flex-none w-72 relative select-none"
             >
-              <div className="relative h-64 w-full">
+              <div className="relative h-80 w-full">
                 <img 
                   src={facility.image} 
                   alt={facility.name} 
                   className="w-full h-full object-cover"
                   draggable="false"
                 />
-                <div className="absolute bottom-0 left-0 p-3 bg-black bg-opacity-30 w-full">
-                  <p className="text-white text-sm">{facility.name}</p>
+                <div className="absolute bottom-0 left-0 p-3 bg-black bg-opacity-50 w-full">
+                  <p className="text-white font-bold  text-left text-sm">{facility.name}</p>
                 </div>
               </div>
             </div>
