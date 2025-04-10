48012b921d0f4a4f0c0b9d7be73f1989741845a3
Dawood Khan <110541711+Iyisku@users.noreply.github.com>
2025-04-10 16:46:22 +0500

    2 pages left

--- client/src/components/Home/Customers.js
+++ client/src/components/Home/Customers.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Customers() {
   return (
-    <div>Customers</div>
+    <div>
+      <div className='bg-[#D0E4FF] h-[600px]'>
+        <h1 className='text-center font-jeju pt-16 text-4xl'>Our Satisfied Visitors</h1>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Home/Location.js
+++ client/src/components/Home/Location.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Location() {
   return (
-    <div>Location</div>
+    <div>
+      <div className='h-[400px]'>
+
+      </div>
+    </div>
   )
 }
 
