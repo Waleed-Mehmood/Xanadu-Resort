e1f139eddf1a717b6584139fae9c3b0b6795c02a
Dawood Khan <110541711+Iyisku@users.noreply.github.com>
2025-04-17 12:07:11 +0500

    roouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Lodging from './pages/Lodging';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lodging" element={<Lodging />} />
+      </Routes>
     </div>
   );
 }
