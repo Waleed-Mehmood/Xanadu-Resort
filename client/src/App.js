6c76745c8c2e38f579813c412152ac79dd91a71b
Dawood Khan <110541711+Iyisku@users.noreply.github.com>
2025-04-17 12:04:35 +0500

    Revert "React Router"
    This reverts commit afe4d4eab5b5df6a062325c8b54442d344d7cb58.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Lodging from './pages/Lodging';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lodging" element={<Lodging />} />
-      </Routes>
+      <Home/>
     </div>
   );
 }
