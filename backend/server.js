70ef43de2a58002e92618b9649b952e78f19be3b
Dawood Khan <110541711+Iyisku@users.noreply.github.com>
2025-04-22 15:21:37 +0500

    mogodb

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,28 @@ const adminRoute = require('./routes/adminRoute');
 const userRoutes = require('./routes/userRoutes');
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+
+//env variables
 const PORT = process.env.PORT || 5000;
+const MongoUri = process.env.MONGO_URI;
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//MongoDB connection
+mongoose.connect(MongoUri).then(() => {
+  console.log('MongoDB connected successfully!');
+}
+).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+
+//Routes
 app.use('/admin', adminRoute);
 app.use('/user', userRoutes);
-
 app.get('/', (req, res) => {
   res.send('Hello from Node.js!');
 });
